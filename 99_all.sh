b5c95bf305c7d6e060c37cb08adbbea6808246bf
Roman Pietrzak <y@ke.mu>
2020-09-03 06:25:59 +0100

    Minor build script improvement: exit on fail

--- 99_all.sh
+++ 99_all.sh
@@ -1,3 +1,6 @@
-./01_prepare.sh
-./02_build.sh
-./03_test.sh
+# Make shell script exit on failures and add some verbosity
+set -exv
+
+source ./01_prepare.sh
+source ./02_build.sh
+source ./03_test.sh
