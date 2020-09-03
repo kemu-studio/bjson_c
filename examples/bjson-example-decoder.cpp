fd88c36220a71135e895a20dd39cce008ca575cc
Sylwester Wysocki <dzik@ke.mu>
2020-09-03 06:17:52 +0200

    Added C++ examples.

---
+++ examples/bjson-example-decoder.cpp
@@ -0,0 +1,226 @@
+/*
+ * Copyright (c) 2017 by Kemu Studio (visit ke.mu)
+ *
+ * Author(s): Sylwester Wysocki <sw@ke.mu>,
+ *            Roman Pietrzak <rp@ke.mu>
+ *
+ * This file is a part of the KEMU Binary JSON library.
+ * See http://bjson.org for more.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+ * This is a basic example showing how to decode input BJSON stream into
+ * list of tokens.
+ */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <bjson/BjsonDecoder.hpp>
+
+#ifdef WIN32
+# include <io.h>
+# include <fcntl.h>
+#endif /* WIN32 */
+
+#define BUFFER_SIZE 1024
+
+static int g_deepIdx = 0;
+
+class MyBjsonDecoder : public BjsonDecoder
+{
+  int _deepIdx;
+
+  //
+  // Helper function to pretty format decoded tokens.
+  //
+
+  void _deep_printf(const char *fmt, ...)
+  {
+    int i;
+
+    va_list args;
+
+    /*
+     * Print deep spaces first to show tree structure.
+     */
+
+    for (i = 0; i < _deepIdx * 2; i++)
+    {
+      printf(" ");
+    }
+
+    /*
+     * Print caller specified message.
+     */
+
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
+
+    printf("\n");
+  }
+
+  // ---------------------------------------------------------------------------
+  // Callback functions called when next token was successfuly decoded.
+  // We use these functions to tracks what is going on while deciding.
+  // ---------------------------------------------------------------------------
+
+  int onNull()
+  {
+    _deep_printf("null");
+    return 1;
+  }
+
+  int onBoolean(int value)
+  {
+    _deep_printf("boolean (%s)", value ? "true" : "false");
+    return 1;
+  }
+
+  int onInteger(long long value)
+  {
+    _deep_printf("integer (%lld)", value);
+    return 1;
+  }
+
+  int onDouble(double value)
+  {
+    _deep_printf("double (%lf)", value);
+    return 1;
+  }
+
+  int onString(const unsigned char *text, size_t textLen)
+  {
+    _deep_printf("string ('%.*s')", textLen, text);
+    return 1;
+  }
+
+  int onMapKey(const unsigned char *text, size_t textLen)
+  {
+    deep_printf("key ('%.*s')", textLen, text);
+    return 1;
+  }
+
+  int onStartMap()
+  {
+    _deep_printf("{");
+    _deepIdx++;
+    return 1;
+  }
+
+  int onEndMap()
+  {
+    _deepIdx--;
+    _deep_printf("}");
+    return 1;
+  }
+
+  int onStartArray()
+  {
+    _deep_printf("[");
+    _deepIdx++;
+    return 1;
+}
+
+  int onEndArray()
+  {
+    _deepIdx--;
+    _deep_printf("]");
+    return 1;
+  }
+
+  public:
+
+  MyBjsonDecoder : _deepIdx(0) {}
+};
+
+
+/* ----------------------------------------------------------------------------
+ *                                 Entry point
+ * ---------------------------------------------------------------------------*/
+
+int main()
+{
+  uint8_t bjsonBuffer[BUFFER_SIZE] = {0};
+
+  size_t bytesReaded = 0;
+
+  int goOn = 1;
+
+  //
+  // Set stdin to binary mode on windows. We're going to read raw
+  // binary buffer (input bjson stream).
+  //
+
+  #ifdef WIN32
+  _setmode(0, _O_BINARY);
+  freopen(NULL, "rb", stdin);
+  #endif
+
+  //
+  // Create new decoder context.
+  //
+
+  MyBjsonDecoder bjsonDecoder;
+
+  //
+  // Read BJSON from stdin by <BUFFER_SIZE> chunks.
+  //
+
+  while(goOn)
+  {
+    // Read next BJSON chunk.
+    bytesReaded = fread(bjsonBuffer, 1, BUFFER_SIZE, stdin);
+
+    if (bytesReaded > 0)
+    {
+      // Pass readed chunk to decoder.
+      if (bjsonDecoder.parse(bjsonBuffer, bytesReaded) != bjson_status_ok)
+      {
+        // Data passed, but decode process failed. Don't go on anymore.
+        goOn = 0;
+      }
+    }
+    else
+    {
+      // EOF or error. Don't go on anymore.
+      goOn = 0;
+
+      if (!feof(stdin))
+      {
+        fprintf(stderr, "ERROR: Can't read from input stream.\nError code is: %d", errno);
+      }
+    }
+  }
+
+  //
+  // All input data processed. Tell the library, that we passed
+  // while expected bytes. This step is needed to detect unterminated
+  // BJSON stream e.g. in the middle of array.
+  //
+
+  if (bjsonDecoder.complete(decodeCtx) != bjson_status_ok)
+  {
+    //
+    // Error - whole buffer processed, but something wrong while decoding.
+    // Check what is going on.
+    //
+
+    printf("parse error: %s\n", bjsonDecoder.formatErrorMessage(1).c_str());
+  }
+
+  return 0;
+}
---
+++ examples/bjson-example-encoder.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2017,2020 by Kemu Studio (visit ke.mu)
+ *
+ * Author(s): Sylwester Wysocki <sw@ke.mu>,
+ *            Roman Pietrzak <rp@ke.mu>
+ *
+ * This file is a part of the KEMU Binary JSON library.
+ * See http://bjson.org for more.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <errno.h>
+#include <bjson/BjsonEncoder.hpp>
+
+#ifdef WIN32
+# include <io.h>
+# include <fcntl.h>
+#endif /* WIN32 */
+
+int main()
+{
+  void  *bjsonBuffer     = NULL;
+  size_t bjsonBufferSize = 0;
+
+  /*
+   * Set stdout to binary mode on windows. We're going to print out
+   * raw binary buffer (output bjson stream).
+   */
+
+  #ifdef WIN32
+  _setmode(1, _O_BINARY);
+  freopen(NULL, "wb", stdout);
+  #endif
+
+  /*
+   * Create new encoder object.
+   */
+
+  BjsonEncoder bjsonEncoder;
+
+  /*
+   * Let's encode some tokens now!
+   * We can use bjson_encodeXxx() functions.
+   * We're going to encode below JSON document:
+   * {
+   *   "key1" : ["Text example", 1234, 3.14],
+   *   "key2" : true,
+   *   "key3" : null
+   * }
+   */
+
+  bjsonEncoder.encodeMapOpen();
+
+    /*
+     * "key1" : ["Text example", 1234, 3.14]
+     */
+
+    bjsonEncoder.encodeCString("key1");
+    bjsonEncoder.encodeArrayOpen();
+      bjsonEncoder.encodeCString("Text example");
+      bjsonEncoder.encodeInteger(1234);
+      bjsonEncoder.encodeDouble(3.14d);
+    bjsonEncoder.encodeArrayClose();
+
+    /*
+     * "key2" : true
+     */
+
+    bjsonEncoder.encodeCString("key2");
+    bjsonEncoder.encodeBool(1);
+
+    /*
+     * "key2" : true
+     */
+
+    bjsonEncoder.encodeCString("key3");
+    bjsonEncoder.encodeNull();
+
+  bjsonEncoder.encodeMapClose();
+
+  /*
+   * BJSON encoded. Now get pointers to created buffer.
+   */
+
+  if (bjsonEncoder.getResult(&bjsonBuffer,
+                             &bjsonBufferSize) == bjson_status_ok)
+  {
+    /*
+     * Success, put encoded binary buffer to stdout.
+     */
+
+    fwrite(bjsonBuffer, bjsonBufferSize, 1, stdout);
+  }
+  else
+  {
+    /*
+     * Error while encoding. Show what is going on.
+     */
+
+    fprintf(stderr, "%s", bjsonEncoder.formatErrorMessage(1).c_str());
+  }
+
+  return 0;
+}
