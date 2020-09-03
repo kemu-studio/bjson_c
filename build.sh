102943704538df082a52b8f92c26f222b9c9417c
Roman Pietrzak <y@ke.mu>
2020-09-03 06:08:58 +0100

    Fixed build and more gitignore

--- build.sh
+++ build.sh
@@ -4,18 +4,19 @@ mkdir -p build/lib
 mkdir -p build/include
 
 # Build library.
-cd src
+echo "- Building library"
 make # VERBOSE=1
 make install
-cd ..
 
 # Build tests.
+echo "- Building tests"
 cd tests
 make # VERBOSE=1
 make install
 cd ..
 
 # Build examples.
+echo "- Building examples"
 cd examples
 make # VERBOSE=1
 make install
